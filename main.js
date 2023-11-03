49d42635ee0cd3b89bf696ee756f8f8e04578535
japoon999 <aziz.azizli.95@inbox.ru>
2023-11-03 17:11:59 +0400

    age control

---
+++ main.js
@@ -0,0 +1,14 @@
+function contorolAge(name) {
+    var yas = prompt("Yasinizi daxil edin: (Yasiniz 18 yasdan boyuk olmalidir)");
+ if (yas>18) {
+    alert("salam xos gelmisiniz:" + " " + name )
+ }else {
+    alert("Siz sayta daxil ola bilmersiniz!")
+ }
+}
+
+contorolAge("Aziz");
+
+
+
+
